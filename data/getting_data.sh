bdf3f35535201f386ad25eace325b36be3b2c345
Christophe Pouzat <christophe.pouzat@parisdescartes.fr>
2017-03-07 13:41:15 +0100

    Better data format and one additional data set.

--- data/getting_data.sh
+++ data/getting_data.sh
@@ -2,6 +2,11 @@
 # The Statistical Analysis of Series of Events (1966) are available
 # From Larry Wasserman web site (the are expressed in seconds).
 wget http://www.stat.cmu.edu/~larry/all-of-nonpar/=data/nerve.dat
+sed -e "s/\t/\n/g" < nerve.dat > nerve2.dat
+sed -e "s/^M//g" < nerve2.dat > nerve3.dat
+rm nerve.dat nerve2.dat
+mv nerve3.dat nerve.dat
+
 
 # Traffic data of Bartlett are available from Jim Lindsey's web
 # site
@@ -10,5 +15,9 @@ tar -xvzf stoch2.tgz ch4/vehicles.dat
 mv ch4/vehicles.dat .
 rmdir ch4
 rm stoch2.tgz
+sed '1d' vehicles.dat | sed 's/^\W//g' | sed 's/^M//g' | sed 's/ /\n/g' | grep '[^[:blank:]]' > traffic.dat
+rm vehicles.dat
+
+
 
 
